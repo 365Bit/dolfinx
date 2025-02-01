91ac2f67fdf278eb6db99680274b1f27088cc6d4
365Bit <20995752+365Bit@users.noreply.github.com>
2025-02-01 21:17:36 +0100

    Adds generated stub

---
+++ python/dolfinx/cpp.pyi
@@ -0,0 +1,11 @@
+from . import (
+    common as common,
+    fem as fem,
+    geometry as geometry,
+    graph as graph,
+    io as io,
+    la as la,
+    log as log,
+    mesh as mesh,
+    refinement as refinement
+)
